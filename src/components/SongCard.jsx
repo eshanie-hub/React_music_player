67e5b6e204ab0734dcb00fb3116d305284f328d1
eshanie <eshaniechathurika@gmail.com>
2023-09-10 21:25:18 +0530

    update

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -35,7 +35,7 @@ const SongCard = ({ song, isPlaying, activeSong, data, i, isLike, check, is }) =
     <div className="pt-6">
       <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer"> 
         <div className="relative w-full h-55 group">
-          <div className={`absolute inset-0 justify-center items-center bg-black rounded-lg bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-white bg-opacity-70' : 'hidden'}`}>
+          <div className={`absolute inset-0 justify-center items-center bg-white rounded-lg bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-white bg-opacity-60' : 'hidden'}`}>
           
             <PlayPause 
               isPlaying={isPlaying}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
     <div className="w-full p-16">
       <h2 className="text-2xl font-bold">Home</h2>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8 ">
-      {data?.tracks.map((song, i, o,) => (
+      {data?.tracks.map((song, i) => (
         <SongCard 
           key={song.key}
           song={song}
